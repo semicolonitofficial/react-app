f30cab92c7ba25797f999c996a9ba3a7d62af599
masuddev2 <tuhin.semicolonit@gmail.com>
2025-10-29 11:05:39 +0600

    first

---
+++ components/NavBar.jsx
@@ -0,0 +1,14 @@
+import Button from "./Button";
+
+export default function NavBar() {
+  return (
+    <div>
+      <span>Logo</span>
+      <ul>
+        <li>Home</li>
+        <li>Contact</li>
+      </ul>
+      <Button />
+    </div>
+  );
+}
---
+++ pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import Banner from "../components/Banner";
+import NavBar from "../components/NavBar";
+
+export default function HomePage() {
+  return (
+    <>
+      <NavBar />
+      <Banner />
+    </>
+  );
+}
